490402089a32239595b7dbebf4c1a52202654eba
Newfile01 <nicolas.morel.01000@gmail.com>
2026-01-08 00:14:39 +0100

    Grand nettoyage repo

--- matrix-bgpsim/docs/gendoc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-DOC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$DOC_DIR" || exit 1 
-
-sphinx-build -b html source build/html
--- matrix-bgpsim/docs/source/matrix_bgpsim.rst
+++
@@ -1,7 +0,0 @@
-matrix\_bgpsim
-======================
-
-.. automodule:: matrix_bgpsim
-   :members:
-   :undoc-members:
-   :show-inheritance:
